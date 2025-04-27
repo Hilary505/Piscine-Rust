47e6322adc2c84641af587715ee07104da278f8d
hilaromondi <omondihilary19@gmail.com>
2025-04-27 18:23:52 +0300

    add queen

---
+++ queens/src/lib.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+pub struct ChessPosition {
+    pub rank: i32,
+    pub file: i32,
+}
+
+#[derive(Debug)]
+pub struct Queen {
+    pub position: ChessPosition,
+}
+
+impl ChessPosition {
+    pub fn new(rank: i32, file: i32) -> Option<Self> {
+        if rank >= 0 && rank < 8 && file >= 0 && file < 8 {
+            Some(ChessPosition { rank, file })
+        } else {
+            None
+        }
+    }
+}
+
+impl Queen {
+    pub fn new(position: ChessPosition) -> Self {
+        Queen { position }
+    }
+
+    pub fn can_attack(&self, other: &Queen) -> bool {
+        let pos1 = &self.position;
+        let pos2 = &other.position;
+
+        pos1.rank == pos2.rank || pos1.file == pos2.file || (pos1.rank - pos2.rank).abs() == (pos1.file - pos2.file).abs()
+    }
+}
